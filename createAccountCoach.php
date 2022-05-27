6f6e1789b7d22d9318d23c4a2654887bdf6a51ed
rmiooone <hermione.carpentier.pro@gmail.com>
2022-05-27 11:24:22 +0200

    inscription coach

--- createAccountCoach.php
+++ createAccountCoach.php
@@ -1,3 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+  
+  </head>
+  <body>
+      <br>
+  
+  <div id="btn">
+    <a href="index.html"><img src="images/acc.png" alt="erreur" width="150" height="40"/></a>
+  </div>
+</body>
+  </html>
+
+
+
+
+
 <?php
 $Prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $nom = isset($_POST["nom"])? $_POST["nom"] : "";
